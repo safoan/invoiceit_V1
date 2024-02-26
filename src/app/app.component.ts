70352dcad5695401e20cfa9df9b5207a66458288
safoan bazine <safibazine@gmail.com>
2024-02-26 00:23:03 +0000

    added some nav-bar but  i will change it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule , Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+//importing componenet 
+import { ProductComponent } from './product/product.component';
+import { UserComponent } from './user/user.component';
+import { InvoiceComponent } from './invoice/invoice.component';
+import { CustomerComponent } from './customer/customer.component';
+
+const routes: Routes = [
+  { path: 'user', component: UserComponent },
+  { path: 'product', component: ProductComponent }, // Ensure you import ProductComponent
+  { path: 'customer', component: CustomerComponent },
+  { path: 'invoice', component: InvoiceComponent }
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatToolbarModule , RouterModule ,MatSidenavModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
